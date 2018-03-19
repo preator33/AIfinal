58a7dd0c04b418ad21f166b9de59b6d495f73318
James <james.bonnyman@uoit.net>
2018-03-19 11:28:43 -0400

    [Commit] Initial commit. first project build, trying not to include info and meta files.

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Snake : MonoBehaviour
+{
+    private Snake next; //Linked List
+    static public Action<String> hit;
+
+    public void SetNext(Snake IN)
+    {
+        next = IN;
+    }
+
+    public Snake GetNext()
+    {
+        return next;
+    }
+
+    public void RemoveTail()
+    {
+        Destroy(this.gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (hit != null)
+        {
+            hit(other.tag);
+        }
+
+        if (other.tag == "Food")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+
+}
